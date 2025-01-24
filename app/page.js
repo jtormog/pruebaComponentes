ceaacb8a8aa4fec00c375ccfcab1fbef7982758a
Josep Tormo <jostorgar4@alu.edu.gva.es>
2025-01-24 17:03:47 +0100

    .

--- app/page.js
+++ app/page.js
@@ -1,101 +1,58 @@
-import Image from "next/image";
+import {
+  CalendarIcon,
+  HomeIcon,
+  GlobeIcon,
+  VideoIcon,
+} from "@radix-ui/react-icons";
 
-export default function Home() {
+import { BentoCard, BentoGrid } from "@/components/ui/bento-grid";
+
+const features = [
+  {
+    Icon: HomeIcon,
+    name: "Inicio",
+    description: "Sección de inicio",
+    href: "/",
+    cta: "Ir al inicio",
+    background: <img src="https://static.vecteezy.com/system/resources/previews/035/952/532/non_2x/house-single-line-continuous-outline-art-drawing-and-simple-one-line-home-minimalist-design-vector.jpg" className=""/>,
+    className: "lg:row-start-1 lg:row-end-3 lg:col-start-2 lg:col-end-2",
+  },
+  {
+    Icon: GlobeIcon,
+    name: "3",
+    description: "Bento card 3",
+    href: "/",
+    cta: "Learn more",
+    background: <img className="absolute -right-20 -top-20 opacity-60" />,
+    className: "lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-2",
+  },
+  {
+    Icon: CalendarIcon,
+    name: "4",
+    description: "Bento card 4",
+    href: "/",
+    cta: "Learn more",
+    background: <img className="absolute -right-20 -top-20 opacity-60" />,
+    className: "lg:col-start-1 lg:col-end-1 lg:row-start-2 lg:row-end-3",
+  },
+  {
+    Icon: VideoIcon,
+    name: "Rick roll",
+    description:
+      "Never gonna give you up",
+    href: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    cta: "Never gonna let you down",
+    background: <img src="rick_astley.jpg" className=" block mx-auto" />,
+    className: "lg:col-start-3 lg:col-end-3 lg:row-start-1 lg:row-end-3 ",
+  },
+];
+
+export default function BentoDemo() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <BentoGrid className="lg:grid-rows-2">
+      {features.map((feature) => (
+        <BentoCard key={feature.name} {...feature} />
+      ))}
+    </BentoGrid>
   );
 }
